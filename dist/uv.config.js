53fa96eae3f1361dc682ea82f70beaebc7105c63
Phucskl08 <duyphuc190108@gmail.com>
2025-04-10 13:46:48 +0700

    update dist/uv.config.js

--- dist/uv.config.js
+++ dist/uv.config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "https://bare-server-node-ilehuycmw-phucs-projects-2a11ed2b.vercel.app/bare/", // không trỏ domain khác
+  prefix: "https://bare-server-node-ilehuycmw-phucs-projects-2a11ed2b.vercel.app/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv.handler.js",
