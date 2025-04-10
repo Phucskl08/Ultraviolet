872b6ba75652572db24f7f001babe79cfdbcb1fd
Phucskl08 <duyphuc190108@gmail.com>
2025-04-10 07:55:57 +0700

    Update uv.config.js

--- dist/uv.config.js
+++ dist/uv.config.js
@@ -1,11 +1,11 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-	prefix: "/service/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/uv.handler.js",
-	client: "/uv.client.js",
-	bundle: "/uv.bundle.js",
-	config: "/uv.config.js",
-	sw: "/uv.sw.js",
+  prefix: "https://bare.share-proxy.ga/service/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv.handler.js",
+  client: "/uv.client.js",
+  bundle: "/uv.bundle.js",
+  config: "/uv.config.js",
+  sw: "/uv.sw.js",
 };
